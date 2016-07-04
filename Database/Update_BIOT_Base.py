4ca625dd6d8e0800f3daf524cef99e015079c59c
gcardoza <geof.cardoza@gmail.com>
2016-07-04 14:18:15 -0400

    Completed logic to read data from all Active Nodes, and  added the command line argument "-d" to turn on debug mode.

--- Database/Update_BIOT_Base.py
+++ Database/Update_BIOT_Base.py
@@ -27,15 +27,24 @@ import sqlite3
 import time
 import datetime
 
-debug = 1
+debug = 0
 activeNodeNumber = []
 activeNodePort = []
+availableNodeData = [0,0,0,0,0,0,0,0]
 serialPort = []
 
+# Check for command line arguments and if debug is on -d
+if (len(sys.argv) >= 2):
+    x = sys.argv[1]
+    if (x == "-d"):
+        debug = 1
+        print("Debug Mode is On")
+    
 # ***** Open and connect to the BIOT SQL Database *****
 try:
     conn = sqlite3.connect('BIOT_Base.db')
-    print ("Opened connection to BIOT_Base database")
+    if (debug == 1): print ("Opened connection to BIOT_Base database")
+    
 except:
     print ("Error connecting to the Database", sys.exc_info()[0])
     raise
@@ -49,12 +58,12 @@ try:
         activeNodeNumber.append(row[0])
         activeNodePort.append(row[1])
         maxNodes += 1
-    print("Total number of Active Nodes = ", maxNodes)
+    if (debug == 1) : print("Total number of Active Nodes = ", maxNodes)
     
     # Open the Serial Ports for each Active Node
     n = 0
     while n < maxNodes:
-        print("Opening Serial Port ", activeNodePort[n], "for Node:", activeNodeNumber[n])
+        if (debug == 1) : print("Opening Serial Port ", activeNodePort[n], "for Node:", activeNodeNumber[n])
         serialPort.append(serial.Serial(activeNodePort[n], timeout=5))
         n += 1
 
@@ -65,98 +74,111 @@ except:
 # ***** Main Program Loop - Process 1 data record from each node and move to next node *****
 currentNode = 0   #start loop on the first node
 while True:
-    try:    # Read next record from the current Node
-        print("Reading Data from Node:", activeNodeNumber[currentNode])
-        record = serialPort[currentNode].readline()
+    try:    # Check if there is data to read from the node port
+        if (debug == 1) : print("Reading Data from Node:", activeNodeNumber[currentNode])
+        availableNodeData[currentNode] = serialPort[currentNode].inWaiting()
+        if (debug == 1) : print("Bytes available on Node = ", availableNodeData[currentNode])
+
+        # Read data if there is data available
+        if(availableNodeData[currentNode] != 0):
+            record = serialPort[currentNode].readline()
 
     except:  # *?* Handle each error condition appropriately
         print("Could not read data from serial port", sys.exc_info()[0])
-        time.sleep(30) # Wait 60 seconds and try again if a device has no data
-        continue
+        raise
 
-    nodeData = record.decode("utf-8")	# Convert the data from binary to a string
-    print ("Input record received from Node ->", nodeData)
+    # 1. Process current record if there is data available
+    if(availableNodeData[currentNode] != 0):
+        nodeData = record.decode("utf-8")	# Convert the data from binary to a string
+        if (debug == 1) : print ("Input record received from Node ->", nodeData)
 
-    # Ensure this is a valid data record (header= RIOT1) and not a debug statement
-    R1 = nodeData[0:5]
-    if (R1 == "RIOT1"):
-        # Parse each data field from the record
-        print ("Parsing Node Data")
-        SW = nodeData[6:11]
-        ID = nodeData[16:18]
-        DS = nodeData[23:33]
-        TS = nodeData[34:42]
-        DT = nodeData[47:52]
-        DH = nodeData[57:62]
-        BT = nodeData[67:72]
-        BP = nodeData[77:82]
-        M1 = nodeData[87:92]
-        M2 = nodeData[97:102]
-        M3 = nodeData[107:112]
-        SE = nodeData[117:123]
-
-        # *?* Override date and time with current date and time until RIOT has a RTC
-        # TS = time.localtime(time.time)
-        # DT = now.datetime.date.today()
+        # Ensure this is a valid data record (header= RIOT1) and not a debug statement
+        R1 = nodeData[0:5]
+        if (R1 == "RIOT1"):
+            # 2. Parse each data field from the record
+            if (debug == 1) : print ("Parsing Node Data")
+            SW = nodeData[6:11]
+            ID = nodeData[16:18]
+            DS = nodeData[23:33]
+            TS = nodeData[34:42]
+            DT = nodeData[47:52]
+            DH = nodeData[57:62]
+            BT = nodeData[67:72]
+            BP = nodeData[77:82]
+            M1 = nodeData[87:92]
+            M2 = nodeData[97:102]
+            M3 = nodeData[107:112]
+            SE = nodeData[117:123]
                
-        # Insert Parsed Node data into the Database
-             # *?* For now I'm using the Pi date - when the Arduino has a RTC switch the update fields
-        try:
-            print ("Inserting Data into Database")
-            conn.execute('''INSERT INTO Sensor_Data (Node_ID, Date_Stamp, Time_Stamp,
-            DHT22_Temperature, DHT22_Humidity, BMP180_Temperature, BMP180_Pressure,
-            Moisture_1, Moisture_2, Moisture_3) \
-            VALUES (?,Date('now'),Time('now'),?,?,?,?,?,?,?)''', (ID, DT, DH, BT, BP, M1, M2, M3));
+            # Insert Parsed Node data into the Database
+                 # *?* For now I'm using the Pi date - when the Arduino has a RTC switch the update fields
+            try:
+                if (debug == 1) : print ("Inserting Data into Database")
+                conn.execute('''INSERT INTO Sensor_Data (Node_ID, Date_Stamp, Time_Stamp,
+                DHT22_Temperature, DHT22_Humidity, BMP180_Temperature, BMP180_Pressure,
+                Moisture_1, Moisture_2, Moisture_3) \
+                VALUES (?,Date('now'),Time('now'),?,?,?,?,?,?,?)''', (ID, DT, DH, BT, BP, M1, M2, M3));
 
-        except:
-            # Insert failed - so Rollback the Insert and close the serial port
-            print ("Rolling back db insert")
-            cursor.rollback()
+            except:
+                # Insert failed - so Rollback the Insert and close the serial port
+                print ("Rolling back db insert")
+                cursor.rollback()
 
-            print ("Closing db connection")
-            conn.close()    # close database connection
+                print ("Closing db connection")
+                conn.close()    # close database connection
 
-            Print("Closing all serial ports")
-            n = 0
-            while (n < maxNodes):
-                serialPort[n].close()
-                n +=1
+                print("Closing all serial ports")
+                n = 0
+                while (n < maxNodes):
+                    serialPort[n].close()
+                    n +=1
                 
-            print("Error Inserting data into the Database", sys.exc_info()[0])
-            raise
+                print("Error Inserting data into the Database", sys.exc_info()[0])
+                raise
 
-        #  Write the data and committ it to the database
-        conn.commit()
+            #  Write the data and committ it to the database
+            conn.commit()
                         
-        # Read back the last written record and print contents
-        try:
-            cursor = conn.execute('''SELECT Node_ID, Date_Stamp, Time_Stamp, DHT22_Temperature,
-            DHT22_Humidity, BMP180_Temperature, BMP180_Pressure, Moisture_1, Moisture_2,
-            Moisture_3 from Sensor_Data WHERE ROWID = (SELECT MAX(ROWID) FROM Sensor_Data)''');
-        except:
-            print("Error Reading the Database", sys.exc_info()[0])
-            raise
+            # Read back the last written record and print contents
+            try:
+                cursor = conn.execute('''SELECT Node_ID, Date_Stamp, Time_Stamp, DHT22_Temperature,
+                DHT22_Humidity, BMP180_Temperature, BMP180_Pressure, Moisture_1, Moisture_2,
+                Moisture_3 from Sensor_Data WHERE ROWID = (SELECT MAX(ROWID) FROM Sensor_Data)''');
+            except:
+                print("Error Reading the Database", sys.exc_info()[0])
+                raise
 
-        for row in cursor:
-            print ("Reading Record back from Database")
-            print ("				  Node Data	Database Output")
-            print ("				  ===========	===============")
-            print ("	Device_Type 		= ", R1, "	","-----")
-            print ("	Software Version	= ", SW, "	","-----")
-            print ("	Node_ID 		= ", ID, "		",row[0])
-            print ("	Date_Stamp 		= ", DS, "	",row[1])
-            print ("	Time_Stamp 		= ", TS, "	",row[2])			
-            print ("	DHT22_Temperature	= ", DT, "	",row[3])
-            print ("	DHT22_Humidity 		= ", DH, "	",row[4])
-            print ("	BMP180_Temperature 	= ", BT, "	",row[5])
-            print ("	BMP180_Pressure 	= ", BP, "	",row[6])
-            print ("	Moisture_1 		= ", M1, "	",row[7])
-            print ("	Moisture_2 		= ", M2, "	",row[8])
-            print ("	Moisture_3 		= ", M3, "	",row[9])
-            print ("	Sequence 		= ", SE, "	","------")			
-            print ("\n\n")
+            for row in cursor:
+                print ("Reading Record back from Database")
+                print ("				  Node Data	Database Output")
+                print ("				  ===========	===============")
+                print ("	Device_Type 		= ", R1, "	","-----")
+                print ("	Software Version	= ", SW, "	","-----")
+                print ("	Node_ID 		= ", ID, "		",row[0])
+                print ("	Date_Stamp 		= ", DS, "	",row[1])
+                print ("	Time_Stamp 		= ", TS, "	",row[2])			
+                print ("	DHT22_Temperature	= ", DT, "	",row[3])
+                print ("	DHT22_Humidity 		= ", DH, "	",row[4])
+                print ("	BMP180_Temperature 	= ", BT, "	",row[5])
+                print ("	BMP180_Pressure 	= ", BP, "	",row[6])
+                print ("	Moisture_1 		= ", M1, "	",row[7])
+                print ("	Moisture_2 		= ", M2, "	",row[8])
+                print ("	Moisture_3 		= ", M3, "	",row[9])
+                print ("	Sequence 		= ", SE, "	","------")			
+                print ("\n\n")
 
     # Increment to next active node if at max then start over
     currentNode += 1
     if (currentNode == maxNodes):
+        #check if there was data available on any node
+        n = 0
+        count = 0
+        while n < maxNodes:
+            count += availableNodeData[n]
+            n += 1
+        if (debug == 1) : print("Data Count = ", count)
+
+        if(count == 0):
+            if (debug == 1) : print("No data available on any active port. Sleeping for 10 seconds")
+            time.sleep(10)
         currentNode = 0
